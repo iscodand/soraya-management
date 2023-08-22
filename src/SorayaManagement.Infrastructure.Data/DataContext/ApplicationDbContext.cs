ebbc6fbedd4d05bc963de40134977dd20f7d5042
iscodand <iscodand@outlook.com>
2023-08-22 15:59:41 -0300

    add: Context and Migrations

---
+++ src/SorayaManagement.Infrastructure.Data/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,73 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SorayaManagement.Domain.Entities;
+
+namespace SorayaManagement.Infrastructure.Data.DataContext
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Meal> Meals { get; set; }
+        public DbSet<PaymentType> PaymentTypes { get; set; }
+        public DbSet<Company> Companies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<User>(entity =>
+            {
+                entity.ToTable(name: "Users");
+            });
+
+            builder.Entity<IdentityRole>(entity =>
+            {
+                entity.ToTable(name: "Roles");
+            });
+            builder.Entity<IdentityUserRole<string>>(entity =>
+            {
+                entity.ToTable("UserRoles");
+            });
+
+            builder.Entity<IdentityUserClaim<string>>(entity =>
+            {
+                entity.ToTable("UserClaims");
+            });
+
+            builder.Entity<IdentityUserLogin<string>>(entity =>
+            {
+                entity.ToTable("UserLogins");
+            });
+
+            builder.Entity<IdentityRoleClaim<string>>(entity =>
+            {
+                entity.ToTable("RoleClaims");
+
+            });
+
+            builder.Entity<IdentityUserToken<string>>(entity =>
+            {
+                entity.ToTable("UserTokens");
+            });
+
+            builder.Entity<PaymentType>().HasData(
+                new PaymentType
+                {
+                    Id = 1,
+                    Description = "PIX"
+                },
+                new PaymentType
+                {
+                    Id = 2,
+                    Description = "Dinheiro"
+                }
+            );
+        }
+    }
+}
