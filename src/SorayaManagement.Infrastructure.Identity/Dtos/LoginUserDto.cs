2c1fdef085075142c89e168e741fffb9b40813c4
iscodand <iscodand@outlook.com>
2023-08-23 16:46:35 -0300

    add: validation for Login Dto

--- src/SorayaManagement.Infrastructure.Identity/Dtos/LoginUserDto.cs
+++ src/SorayaManagement.Infrastructure.Identity/Dtos/LoginUserDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SorayaManagement.Infrastructure.Identity.Dtos
 {
     public class LoginUserDto
     {
         public string UserName { get; set; }
+
+        [DataType(DataType.Password)]
+        [StringLength(32, MinimumLength = 8)]
         public string Password { get; set; }
     }
 }
