c0efb799b7cb380d79864bcc494dad7a0cc5e52b
iscodand <iscodand@outlook.com>
2023-08-29 01:31:16 -0300

    add: home view model

---
+++ src/SorayaManagement/ViewModels/HomeViewModel.cs
@@ -0,0 +1,8 @@
+namespace SorayaManagement.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string UserName { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
