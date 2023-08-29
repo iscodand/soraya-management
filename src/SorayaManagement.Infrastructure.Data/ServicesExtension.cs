a1c0733011af2b4a027f441fdc025818a2d01da8
iscodand <iscodand@outlook.com>
2023-08-29 01:28:53 -0300

    add: user repository dependency injection

--- src/SorayaManagement.Infrastructure.Data/ServicesExtension.cs
+++ src/SorayaManagement.Infrastructure.Data/ServicesExtension.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SorayaManagement.Infrastructure.Data.Contracts;
 using SorayaManagement.Infrastructure.Data.DataContext;
+using SorayaManagement.Infrastructure.Data.Repositories;
 
 namespace SorayaManagement.Infrastructure.Data
 {
@@ -9,10 +11,14 @@ namespace SorayaManagement.Infrastructure.Data
     {
         public static IServiceCollection AddDatabaseContext(this IServiceCollection services, IConfiguration configuration)
         {
+            // Database Context
             services.AddDbContext<ApplicationDbContext>(options =>
             {
                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
             });
+
+            // Repositories Dependency Injection
+            services.AddScoped<IUserRepository, UserRepository>();
 
             return services;
         }
