de9b3f4629f2396345c39d3ab0433096879d496e
iscodand <iscodand@outlook.com>
2023-09-01 17:25:52 -0300

    add: Id to GetCustomersViewModel

--- src/SorayaManagement/ViewModels/Customer/GetCustomersViewModel.cs
+++ src/SorayaManagement/ViewModels/Customer/GetCustomersViewModel.cs
@@ -2,6 +2,7 @@ namespace SorayaManagement.ViewModels
 {
     public class GetCustomersViewModel
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string CreatedBy { get; set; }
     }
