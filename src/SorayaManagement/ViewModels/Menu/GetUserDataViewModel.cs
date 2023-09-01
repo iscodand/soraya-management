8e44f813f37968397004aeb29cee5dba8dbee5f5
iscodand <iscodand@outlook.com>
2023-09-01 17:25:37 -0300

    add: GetUserDataViewModel

---
+++ src/SorayaManagement/ViewModels/Menu/GetUserDataViewModel.cs
@@ -0,0 +1,8 @@
+namespace SorayaManagement.ViewModels
+{
+    public class GetUserDataViewModel
+    {
+        public string Name { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
