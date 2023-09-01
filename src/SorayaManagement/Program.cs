fdb1f02091f088080defaadbe901a9f02879741b
iscodand <iscodand@outlook.com>
2023-09-01 17:26:35 -0300

    add: Newtonsoft.Json settings // Session settings

--- src/SorayaManagement/Program.cs
+++ src/SorayaManagement/Program.cs
@@ -3,12 +3,21 @@ using SorayaManagement.Infrastructure.Identity;
 using SorayaManagement.Infrastructure.Identity.Contracts;
 using SorayaManagement.Services;
 using SorayaManagement.Application;
+using Newtonsoft.Json;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure Newtonsoft.Json to Ignore Loops
+JsonConvert.DefaultSettings = () => new JsonSerializerSettings
+{
+    Formatting = Formatting.Indented,
+    ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+};
 
 // Dependency Injection
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 builder.Services.AddScoped<IAuthenticatedUserService, AuthenticatedUserService>();
+builder.Services.AddScoped<ISessionService, SessionService>();
 
 // Setting up ApplicationLayer
 builder.Services.AddApplicationServices();
@@ -22,6 +31,13 @@ builder.Services.AddIdentitySetup();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -33,6 +49,8 @@ if (!app.Environment.IsDevelopment())
 }
 
 // app.UseHttpsRedirection();
+
+app.UseSession();
 
 app.UseStaticFiles();
 
