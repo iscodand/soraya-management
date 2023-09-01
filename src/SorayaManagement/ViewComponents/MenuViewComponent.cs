7fd0751e25e9ad2244172221b89a715b1d341a1f
iscodand <iscodand@outlook.com>
2023-09-01 17:24:50 -0300

    refactor: remove authenticated user service and add session service

--- src/SorayaManagement/ViewComponents/MenuViewComponent.cs
+++ src/SorayaManagement/ViewComponents/MenuViewComponent.cs
@@ -1,22 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using SorayaManagement.Domain.Entities;
 using SorayaManagement.Infrastructure.Identity.Contracts;
+using SorayaManagement.ViewModels;
 
 namespace SorayaManagement.ViewComponents
 {
     public class MenuViewComponent : ViewComponent
     {
-        private readonly IAuthenticatedUserService _authenticatedUserService;
+        private readonly ISessionService _sessionService;
 
-        public MenuViewComponent(IAuthenticatedUserService authenticatedUserService)
+        public MenuViewComponent(ISessionService sessionService)
         {
-            _authenticatedUserService = authenticatedUserService;
+            _sessionService = sessionService;
         }
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            User authenticatedUser = await _authenticatedUserService.GetAuthenticatedUserAsync();
-            return View(authenticatedUser);
+            User authenticatedUser = _sessionService.RetrieveUserSession();
+
+            GetUserDataViewModel viewModel = new()
+            {
+                Name = authenticatedUser.Name,
+                CompanyName = authenticatedUser.UserCompany.Name,
+            };
+
+            return View(viewModel);
         }
     }
 }
