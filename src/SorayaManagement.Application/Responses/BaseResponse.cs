99236248ca8d8a21fa9a761d5da93eef3d5ddee8
iscodand <iscodand@outlook.com>
2023-09-10 17:54:22 -0300

    add: BaseResponse for application layer

--- src/SorayaManagement.Application/Responses/BaseResponse.cs
+++ src/SorayaManagement.Application/Responses/BaseResponse.cs
@@ -1,4 +1,8 @@
 namespace SorayaManagement.Application.Responses
 {
-
+    // improve this inheritance
+    public class BaseResponse<T> : Infrastructure.Identity.Responses.BaseResponse
+    {
+        public T Data { get; set; }
+    }
 }
