add0540e7cbf0a3dadb2d8cb6fadc2e5ea36699a
iscodand <iscodand@outlook.com>
2023-09-10 17:53:12 -0300

    add: more queries for Orders

--- src/SorayaManagement.Infrastructure.Data/Contracts/IOrderRepository.cs
+++ src/SorayaManagement.Infrastructure.Data/Contracts/IOrderRepository.cs
@@ -4,6 +4,9 @@ namespace SorayaManagement.Infrastructure.Data.Contracts
 {
     public interface IOrderRepository : IGenericRepository<Order>
     {
+        public Task<Order> GetOrderDetailsAsync(int orderId);
         public Task<ICollection<Order>> GetOrdersByCompanyAsync(int companyId);
+        public Task<ICollection<Order>> GetOrdersAlreadyPaidAsync(int companyId);
+        public Task<ICollection<Order>> GetOrdersNotPaidAsync(int companyId);
     }
 }
--- src/SorayaManagement.Infrastructure.Data/Repositories/OrderRepository.cs
+++ src/SorayaManagement.Infrastructure.Data/Repositories/OrderRepository.cs
@@ -14,12 +14,50 @@ namespace SorayaManagement.Infrastructure.Data.Repositories
             _orders = context.Orders;
         }
 
+        public async Task<Order> GetOrderDetailsAsync(int orderId)
+        {
+            return await _orders
+                   .Include(x => x.User).AsNoTracking()
+                   .Include(x => x.Meal).AsNoTracking()
+                   .Include(x => x.Customer).AsNoTracking()
+                   .Include(x => x.PaymentType).AsNoTracking()
+                   .Where(x => x.Id == orderId)
+                   .FirstOrDefaultAsync();
+        }
+
         public async Task<ICollection<Order>> GetOrdersByCompanyAsync(int companyId)
         {
             return await _orders.AsNoTracking()
                                .Include(x => x.User).AsNoTracking()
                                .Include(x => x.Company).AsNoTracking()
+                               .Include(x => x.Meal).AsNoTracking()
+                               .Include(x => x.Customer).AsNoTracking()
+                               .Include(x => x.PaymentType).AsNoTracking()
                                .Where(x => x.CompanyId == companyId)
+                               .ToListAsync();
+        }
+
+        public async Task<ICollection<Order>> GetOrdersAlreadyPaidAsync(int companyId)
+        {
+            return await _orders.AsNoTracking()
+                               .Include(x => x.User).AsNoTracking()
+                               .Include(x => x.Company).AsNoTracking()
+                               .Include(x => x.Meal).AsNoTracking()
+                               .Include(x => x.Customer).AsNoTracking()
+                               .Include(x => x.PaymentType).AsNoTracking()
+                               .Where(x => x.CompanyId == companyId && x.IsPaid == true)
+                               .ToListAsync();
+        }
+
+        public async Task<ICollection<Order>> GetOrdersNotPaidAsync(int companyId)
+        {
+            return await _orders.AsNoTracking()
+                               .Include(x => x.User).AsNoTracking()
+                               .Include(x => x.Company).AsNoTracking()
+                               .Include(x => x.Meal).AsNoTracking()
+                               .Include(x => x.Customer).AsNoTracking()
+                               .Include(x => x.PaymentType).AsNoTracking()
+                               .Where(x => x.CompanyId == companyId && x.IsPaid == false)
                                .ToListAsync();
         }
     }
