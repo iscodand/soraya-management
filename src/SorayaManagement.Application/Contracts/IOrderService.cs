14926e542ea9c0d0780afb12670b902c48eca74e
iscodand <iscodand@outlook.com>
2023-09-10 17:59:41 -0300

    add: more functions for OrderService

--- src/SorayaManagement.Application/Contracts/IOrderService.cs
+++ src/SorayaManagement.Application/Contracts/IOrderService.cs
@@ -1,12 +1,15 @@
-using SorayaManagement.Application.Dtos;
+using SorayaManagement.Application.Dtos.Order;
+using SorayaManagement.Application.Responses;
 using SorayaManagement.Domain.Entities;
-using SorayaManagement.Infrastructure.Identity.Responses;
 
 namespace SorayaManagement.Application.Contracts
 {
     public interface IOrderService
     {
-        public Task<BaseResponse> CreateOrderAsync(CreateOrderDto createOrderDto, User authenticatedUser);
+        public Task<BaseResponse<Order>> CreateOrderAsync(CreateOrderDto createOrderDto, User authenticatedUser);
         public Task<ICollection<Order>> GetOrdersByCompanyAsync(int companyId);
+        public Task<BaseResponse<Order>> GetOrderDetailsAsync(int orderId, User authenticatedUser);
+        public Task<BaseResponse<Order>> MakeOrderPaymentAsync(int orderId, User authenticatedUser);
+        public Task<ICollection<PaymentType>> GetPaymentTypesAsync();
     }
 }
