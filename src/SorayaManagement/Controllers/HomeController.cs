fe42698d0db99685b5465547d05ec30c03923c93
iscodand <iscodand@outlook.com>
2023-09-10 17:59:07 -0300

    add: funcionalities for controllers

--- src/SorayaManagement/Controllers/HomeController.cs
+++ src/SorayaManagement/Controllers/HomeController.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SorayaManagement.Domain.Entities;
 using SorayaManagement.Infrastructure.Identity.Contracts;
-using SorayaManagement.Infrastructure.Identity.Dtos;
 
 namespace SorayaManagement.Controllers
 {
--- src/SorayaManagement/Controllers/OrderController.cs
+++ src/SorayaManagement/Controllers/OrderController.cs
@@ -1,10 +1,11 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SorayaManagement.Application.Contracts;
-using SorayaManagement.Application.Dtos;
+using SorayaManagement.Application.Dtos.Order;
+using SorayaManagement.Application.Responses;
 using SorayaManagement.Domain.Entities;
 using SorayaManagement.Infrastructure.Identity.Contracts;
-using SorayaManagement.Infrastructure.Identity.Responses;
+using SorayaManagement.ViewModels;
 
 namespace SorayaManagement.Controllers
 {
@@ -13,27 +14,90 @@ namespace SorayaManagement.Controllers
     public class OrderController : Controller
     {
         private readonly IOrderService _orderService;
+        private readonly ICustomerService _customerService;
+        private readonly IMealService _mealService;
         private readonly ISessionService _sessionService;
 
-        public OrderController(IOrderService orderService, ISessionService sessionService)
+        public OrderController(IOrderService orderService,
+                               ICustomerService customerService,
+                               IMealService mealService,
+                               ISessionService sessionService)
         {
             _orderService = orderService;
             _sessionService = sessionService;
+            _customerService = customerService;
+            _mealService = mealService;
         }
 
         [HttpGet]
         [Route("")]
-        public IActionResult Orders()
+        public async Task<IActionResult> Orders(string isPaid)
         {
-            // to-do: get all orders by company
-            return View();
+            User authenticatedUser = _sessionService.RetrieveUserSession();
+
+            ICollection<Order> orders = await _orderService.GetOrdersByCompanyAsync(authenticatedUser.CompanyId);
+            List<GetOrdersViewModel> viewModelCollection = new();
+
+            foreach (Order order in orders)
+            {
+                GetOrdersViewModel viewModel = new()
+                {
+                    Id = order.Id,
+                    Description = order.Description,
+                    Price = order.Price,
+                    IsPaid = order.IsPaid,
+                    PaidAt = order.PaidAt,
+                    PaymentType = order.PaymentType.Description,
+                    Meal = order.Meal.Description,
+                    Customer = order.Customer.Name,
+                    CreatedBy = order.User.Name,
+                    CreatedAt = order.CreatedAt
+                };
+
+                viewModelCollection.Add(viewModel);
+            }
+
+            // filtering stuff 
+            // => maybe this should not be here
+            // => verify the best implementation for this
+            switch (isPaid)
+            {
+                case "true":
+                    viewModelCollection = viewModelCollection.Where(x => x.IsPaid == true).ToList();
+                    break;
+                case "false":
+                    viewModelCollection = viewModelCollection.Where(x => x.IsPaid == false).ToList();
+                    break;
+                case "all":
+                    break;
+            }
+
+            return View(viewModelCollection);
         }
 
         [HttpGet]
         [Route("novo/")]
-        public IActionResult Create()
+        public async Task<IActionResult> Create()
         {
-            return View();
+            User authenticatedUser = _sessionService.RetrieveUserSession();
+
+            ICollection<PaymentType> paymentTypes = await _orderService.GetPaymentTypesAsync();
+            ICollection<Customer> customers = await _customerService.GetCustomersByCompanyAsync(authenticatedUser.CompanyId);
+            ICollection<Meal> meals = await _mealService.GetMealsByCompanyAsync(authenticatedUser.CompanyId);
+
+            CreateOrderDropdown createOrderDropdown = new()
+            {
+                PaymentTypes = paymentTypes,
+                Customers = customers,
+                Meals = meals
+            };
+
+            CreateOrderDto createOrderDto = new()
+            {
+                CreateOrderDropdown = createOrderDropdown
+            };
+
+            return View(createOrderDto);
         }
 
         [HttpPost]
@@ -43,17 +107,55 @@ namespace SorayaManagement.Controllers
             if (ModelState.IsValid)
             {
                 User authenticatedUser = _sessionService.RetrieveUserSession();
-                BaseResponse result = await _orderService.CreateOrderAsync(createOrderDto, authenticatedUser);
+                BaseResponse<Order> result = await _orderService.CreateOrderAsync(createOrderDto, authenticatedUser);
                 ViewData["Message"] = result.Message;
 
                 if (result.IsSuccess)
                 {
                     ViewData["IsSuccess"] = true;
-                    return View();
+                    return RedirectToAction("Orders");
                 }
             }
 
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> MarkOrderAsPaid(int orderId)
+        {
+            if (ModelState.IsValid)
+            {
+                User authenticatedUser = _sessionService.RetrieveUserSession();
+                BaseResponse<Order> result = await _orderService.MakeOrderPaymentAsync(orderId, authenticatedUser);
+                ViewData["Message"] = result.Message;
+
+                if (result.IsSuccess)
+                {
+                    ViewData["IsSuccess"] = true;
+                    return RedirectToAction("Orders");
+                }
+            }
+
+            return RedirectToAction("Orders");
+        }
+
+        [HttpGet]
+        [Route("detalhes/{orderId}")]
+        public async Task<IActionResult> Details(int orderId)
+        {
+            if (ModelState.IsValid)
+            {
+                User authenticatedUser = _sessionService.RetrieveUserSession();
+                BaseResponse<Order> result = await _orderService.GetOrderDetailsAsync(orderId, authenticatedUser);
+
+                if (result.IsSuccess)
+                {
+                    return View(result.Data);
+                }
+            }
+
+            return RedirectToAction(nameof(Orders));
         }
     }
 }
