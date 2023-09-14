d63032da26dc5c8561d43b087232594766d13181
iscodand <iscodand@outlook.com>
2023-09-14 02:40:02 -0300

    add: DataCollection field to BaseResponse

--- src/SorayaManagement.Application/Responses/BaseResponse.cs
+++ src/SorayaManagement.Application/Responses/BaseResponse.cs
@@ -3,8 +3,9 @@ namespace SorayaManagement.Application.Responses
     public class BaseResponse<T>
     {
         public string Message { get; set; }
-        public IEnumerable<string> Errors { get; set; }
         public bool IsSuccess { get; set; }
         public T Data { get; set; }
+        public ICollection<T> DataCollection { get; set; }
+        public IEnumerable<string> Errors { get; set; }
     }
 }
