2dad1c92939279ac0a0bb6d377d8863524d96abc
iscodand <iscodand@outlook.com>
2023-09-14 02:39:35 -0300

    fix: add util fields to DTO's

--- src/SorayaManagement.Application/Dtos/Order/CreateOrderDto.cs
+++ src/SorayaManagement.Application/Dtos/Order/CreateOrderDto.cs
@@ -4,11 +4,10 @@ namespace SorayaManagement.Application.Dtos.Order
     {
         public string Description { get; set; }
         public decimal Price { get; set; }
-        public bool IsPaid { get; set; }
-        public DateTime PaidAt { get; set; }
-        public int PaymentTypeId { get; set; }
         public int MealId { get; set; }
         public int CustomerId { get; set; }
-        public CreateOrderDropdown CreateOrderDropdown { get; set; }
+        public int PaymentTypeId { get; set; }
+        public int CompanyId { get; set; }
+        public string UserId { get; set; }
     }
 }
