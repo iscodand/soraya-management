473d37379e34a465d73adfc7e0f1c0220ce15e50
iscodand <iscodand@outlook.com>
2023-09-18 10:27:57 -0300

    update: add more fields to detail customer view model

--- src/SorayaManagement/ViewModels/Customer/DetailCustomerViewModel.cs
+++ src/SorayaManagement/ViewModels/Customer/DetailCustomerViewModel.cs
@@ -1,8 +1,12 @@
+using SorayaManagement.Domain.Entities;
+
 namespace SorayaManagement.ViewModels
 {
     public class DetailCustomerViewModel
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string CreatedBy { get; set; }
+        public ICollection<Order> Orders { get; set; }
     }
 }
