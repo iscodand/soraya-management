a5941b5377b74a50c6970591bd5a81102514041d
iscodand <iscodand@outlook.com>
2023-09-27 17:17:03 -0300

    add: update order dto

---
+++ src/SorayaManagement.Application/Dtos/Order/UpdateOrderDto.cs
@@ -0,0 +1,18 @@
+namespace SorayaManagement.Application.Dtos.Order
+{
+    public class UpdateOrderDto
+    {
+        public int OrderId { get; set; }
+
+        public string Description { get; set; }
+        public int CustomerId { get; set; }
+        public int MealId { get; set; }
+        public int PaymentTypeId { get; set; }
+        public decimal Price { get; set; }
+        public bool IsPaid { get; set; }
+        public DateTime? PaidAt { get; set; }
+
+        public int CompanyId { get; set; }
+        public string UserId { get; set; }
+    }
+}
