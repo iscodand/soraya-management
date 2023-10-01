5fff6b89169ae7076b59b21165c1e36ab218c861
iscodand <iscodand@outlook.com>
2023-10-01 17:24:33 -0300

    add: new js files!

---
+++ src/Presentation/wwwroot/js/utils/formatPhoneNumber.js
@@ -0,0 +1,23 @@
+function formatPhoneNumber(inputElementId) {
+    const phoneInput = document.getElementById(inputElementId);
+
+    if (!phoneInput) {
+        console.error(`Element with ID ${inputElementId} not found.`);
+        return;
+    }
+
+    phoneInput.addEventListener('input', function () {
+        let phoneNumber = phoneInput.value;
+        phoneNumber = phoneNumber.replace(/\D/g, '');
+
+        if (phoneNumber.length >= 11) {
+            phoneNumber = `(${phoneNumber.substring(0, 2)}) ${phoneNumber.substring(2, 3)} ${phoneNumber.substring(3, 8)}-${phoneNumber.substring(8)}`;
+        } else if (phoneNumber.length >= 3) {
+            phoneNumber = `(${phoneNumber.substring(0, 2)}) ${phoneNumber.substring(2, 3)} ${phoneNumber.substring(3)}`;
+        }
+
+        phoneInput.value = phoneNumber;
+    });
+}
+
+formatPhoneNumber('customerPhoneInput');
