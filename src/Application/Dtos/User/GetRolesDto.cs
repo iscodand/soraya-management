3f6989c7bcc748cb85547bdd8f1956471c1b58cd
iscodand <iscodand@outlook.com>
2023-10-01 17:19:40 -0300

    change: Role Id to Role Name to create new user

--- src/Application/Dtos/User/GetRolesDto.cs
+++ src/Application/Dtos/User/GetRolesDto.cs
@@ -2,7 +2,7 @@ namespace Application.Dtos.User
 {
     public class GetRolesDto
     {
-        public string Id { get; set; }
+        public string Name { get; set; }
         public string Description { get; set; }
     }
 }
