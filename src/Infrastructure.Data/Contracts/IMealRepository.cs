15e6ab69736e9ff526240b202566b9062cbc52ef
iscodand <iscodand@outlook.com>
2023-10-19 09:38:06 -0300

    feature: add detail meal repository method

--- src/Infrastructure.Data/Contracts/IMealRepository.cs
+++ src/Infrastructure.Data/Contracts/IMealRepository.cs
@@ -6,5 +6,6 @@ namespace Infrastructure.Data.Repositories
     public interface IMealRepository : IGenericRepository<Meal>
     {
         public Task<ICollection<Meal>> GetMealsByCompanyAsync(int companyId);
+        public Task<Meal> DetailMealAsync(int mealId);
     }
 }
