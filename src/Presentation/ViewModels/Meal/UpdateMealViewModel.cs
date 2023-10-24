3c6d10c3051ed22c82534f28e004f7dc70ae9887
iscodand <iscodand@outlook.com>
2023-10-24 08:20:44 -0300

    feature: add validation to update meal viewModel

--- src/Presentation/ViewModels/Meal/UpdateMealViewModel.cs
+++ src/Presentation/ViewModels/Meal/UpdateMealViewModel.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Presentation.ViewModels.Meal
 {
     public class UpdateMealViewModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome do sabor não pode ser vazio")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "O sabor precisa ter no mínimo 3 caracteres e no máximo 50.")]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "Os acompanhamentos não podem ficar em branco")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Os acompanhamentos precisam ter no mínimo 3 caracteres e no máximo 100.")]
         public string Accompaniments { get; set; }
+
         public int UserCompanyId { get; set; }
     }
 }
