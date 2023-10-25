cd979fe4fc1c5eb23c125e031c6e1d456dc131b4
iscodand <iscodand@outlook.com>
2023-10-25 10:10:04 -0300

    feature: meals now are retrieved by date range

--- src/Application/Services/DataService.cs
+++ src/Application/Services/DataService.cs
@@ -25,10 +25,10 @@ namespace Application.Services
         public async Task<BaseResponse<GetDataDto>> GetDataAsync(int companyId, DateTime? initialDate, DateTime? finalDate)
         {
             BaseResponse<GetOrderDto> orders = await _orderService.GetOrdersByDateRangeAsync(companyId, initialDate, finalDate);
-            BaseResponse<GetMealDto> meals = await _mealService.GetMealsByCompanyAsync(companyId);
-            BaseResponse<GetCustomerDto> customers = await _customerService.GetCustomersByCompanyAsync(companyId);
 
-            var ordersPerDay = orders.DataCollection.GroupBy(x => x.CreatedAt.Day).Select(x => x.Key);
+            // todo => select just the last 6 meals/customers with the higher num of orders at range of initial and final date
+            BaseResponse<GetMealDto> meals = await _mealService.GetMealsByDateRangeAsync(companyId, initialDate.Value, finalDate.Value);
+            BaseResponse<GetCustomerDto> customers = await _customerService.GetCustomersByCompanyAsync(companyId);
 
             GetDataDto getDataDto = new()
             {
