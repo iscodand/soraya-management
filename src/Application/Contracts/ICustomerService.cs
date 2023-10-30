d85c7b4a5efeee8fb4233b934c1f8770f80e9a8b
iscodand <iscodand@outlook.com>
2023-10-30 15:07:41 -0300

    feature: add get customers by date range

--- src/Application/Contracts/ICustomerService.cs
+++ src/Application/Contracts/ICustomerService.cs
@@ -10,7 +10,8 @@ namespace Application.Contracts
         public Task<BaseResponse<UpdateCustomerDto>> ActivateCustomerAsync(int customerId, int userCompanyId);
         public Task<BaseResponse<UpdateCustomerDto>> InactivateCustomerAsync(int customerId, int userCompanyId);
         public Task<BaseResponse<GetCustomerDto>> DeleteCustomerAsync(int customerId, int userCompanyId);
-        public Task<BaseResponse<GetCustomerDto>> GetCustomersByCompanyAsync(int companyId);
+        public Task<BaseResponse<GetCustomerDto>> GetCustomersByCompanyAsync(int userCompanyId);
+        public Task<BaseResponse<GetCustomerDto>> GetCustomersByDateRangeAsync(int userCompanyId, DateTime initialDate, DateTime finalDate);
         public Task<BaseResponse<GetCustomerDto>> GetCustomerByIdAsync(int customerId, int userCompanyId);
         public Task<BaseResponse<DetailCustomerDto>> DetailCustomerAsync(int customerId, int userCompanyId);
     }
