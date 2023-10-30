4b5bf42b082913e9f78f2965b86c5f01ddbc0a39
iscodand <iscodand@outlook.com>
2023-10-30 15:10:56 -0300

    feature: add pagination

---
+++ src/Presentation/wwwroot/js/utils/pagination.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    var itemsPerPage = 5;
+    var currentPage = 1;
+    var totalItems = $('.styled-table .table-row').length;
+
+    console.log(totalItems)
+
+    var totalPages = Math.ceil(totalItems / itemsPerPage);
+    $('#totalPages').text(' de ' + totalPages);
+
+    function showItems() {
+        var rows = $('.styled-table .table-row');
+        var startIndex = (currentPage - 1) * itemsPerPage;
+        var endIndex = startIndex + itemsPerPage;
+
+        rows.hide();
+        rows.slice(startIndex, endIndex).show();
+
+        $('#currentPage').text('Página ' + currentPage);
+    }
+
+    $('#nextPage').click(function () {
+        if (currentPage < totalPages) {
+            currentPage++;
+            showItems();
+        }
+    });
+
+    $('#previousPage').click(function () {
+        if (currentPage > 1) {
+            currentPage--;
+            showItems();
+        }
+    });
+
+    showItems();
+});
