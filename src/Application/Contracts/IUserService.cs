6c8d3e92da6d23fc29e484b28e0e6d34359da73a
iscodand <iscodand@outlook.com>
2023-11-02 15:18:03 -0300

    feature: add update user service

--- src/Application/Contracts/IUserService.cs
+++ src/Application/Contracts/IUserService.cs
@@ -8,6 +8,7 @@ namespace Application.Contracts
         public Task<BaseResponse<GetUserDto>> GetUsersByCompanyAsync(int companyId);
         public Task<BaseResponse<DetailUserDto>> DetailUserAsync(string username, int companyId);
         public Task<BaseResponse<GetRolesDto>> GetRolesAsync();
+        public Task<BaseResponse<UpdateUserDto>> UpdateUserAsync(UpdateUserDto updateUserDto);
         public Task<BaseResponse<GetUserDto>> ActivateUserAsync(string username, int companyId);
         public Task<BaseResponse<GetUserDto>> DeactivateUserAsync(string username, int companyId);
     }
