009648a07cd54debaa0c67d1a7506b175d06cdb0
iscodand <iscodand@outlook.com>
2023-11-04 16:55:41 -0300

    feature [code]: add orders by day to make easier to get data

--- src/Application/Dtos/Data/GetDataDto.cs
+++ src/Application/Dtos/Data/GetDataDto.cs
@@ -7,6 +7,7 @@ namespace Application.Dtos.Data
     public class GetDataDto
     {
         public ICollection<GetOrderDto> Orders { get; set; }
+        public IEnumerable<IGrouping<DateTime, GetOrderDto>> OrdersByDayOfWeek { get; set; }
         public ICollection<GetMealDto> Meals { get; set; }
         public ICollection<GetCustomerDto> Customers { get; set; }
     }
