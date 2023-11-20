32728ec627fbca7db4d59a06fce7d563c8a21b67
iscodand <iscodand@outlook.com>
2023-11-20 07:00:49 -0300

    refactor: retirando property Id, pois ela não é usada em lugar nenhum

--- src/Application/Dtos/User/UpdateUserDto.cs
+++ src/Application/Dtos/User/UpdateUserDto.cs
@@ -2,7 +2,6 @@ namespace Application.Dtos.User
 {
     public class UpdateUserDto
     {
-        public string Id { get; set; }
         public string Username { get; set; }
         public string NewUsername { get; set; }
         public string NewEmail { get; set; }
