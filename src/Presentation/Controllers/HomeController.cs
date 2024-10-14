406a00dd52609cba0e07bd34b296d11c6e018e83
iscodand <isco.dev@outlook.com>
2024-10-14 01:36:42 -0300

    feature: refazendo todo o layout do sistema, deixando moderno, limpo e responsivo

--- src/Presentation/Controllers/HomeController.cs
+++ src/Presentation/Controllers/HomeController.cs
@@ -28,7 +28,11 @@ namespace Presentation.Controllers
 
             if (authenticatedUser is not null)
             {
-                return View(authenticatedUser);
+                DateTime today = DateTime.Today.Date;
+
+                var result = await _dataService.GetDataAsync(authenticatedUser.CompanyId, today, today);
+
+                return View(result.Data);
             }
 
             return RedirectToAction("Login", "Authentication");
@@ -39,34 +43,34 @@ namespace Presentation.Controllers
         [Route("data/")]
         public async Task<IActionResult> GetData(string selectedDate, DateTime initialDate)
         {
-            if (ModelState.IsValid)
-            {
-                GetAuthenticatedUserDto authenticatedUser = SessionService.RetrieveUserSession();
+            // if (ModelState.IsValid)
+            // {
+            //     GetAuthenticatedUserDto authenticatedUser = SessionService.RetrieveUserSession();
 
-                DateTime today = DateTime.Today.Date;
+            //     DateTime today = DateTime.Today.Date;
 
-                Dictionary<string, int> dateMappings = new()
-                {
-                    { "today", 0 },
-                    { "lastWeek", -7 },
-                    { "last15Days", -15 },
-                    { "lastMonth", -365 }
-                };
+            //     Dictionary<string, int> dateMappings = new()
+            //     {
+            //         { "today", 0 },
+            //         { "lastWeek", -7 },
+            //         { "last15Days", -15 },
+            //         { "lastMonth", -365 }
+            //     };
 
-                if (dateMappings.TryGetValue(selectedDate, out int daysToSubtract))
-                {
-                    initialDate = today.AddDays(daysToSubtract);
-                }
+            //     if (dateMappings.TryGetValue(selectedDate, out int daysToSubtract))
+            //     {
+            //         initialDate = today.AddDays(daysToSubtract);
+            //     }
 
-                Response<GetDataDto> result = await _dataService.GetDataAsync(authenticatedUser.CompanyId, initialDate, today);
+            //     Response<GetDataDto> result = await _dataService.GetDataAsync(authenticatedUser.CompanyId, initialDate, today);
 
-                if (result.Succeeded)
-                {
-                    return Json(new { success = true, message = result.Message, data = result.Data });
-                }
+            //     if (result.Succeeded)
+            //     {
+            //         return Json(new { success = true, message = result.Message, data = result.Data });
+            //     }
 
-                return Json(new { success = false, message = result.Message });
-            }
+            //     return Json(new { success = false, message = result.Message });
+            // }
 
             return Json(new { success = false, message = "Ocorreu um erro ao processar a solicitação" });
         }
